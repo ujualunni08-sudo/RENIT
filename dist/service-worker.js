30ceeea682d9814804ebf47d151350136c50084e
ujualunni08-sudo <ujualunni08@gmail.com>
2025-08-21 21:34:22 +0530

    Add files via upload

---
+++ dist/service-worker.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  // Placeholder for fetch event logic (e.g., caching)
+}); 
